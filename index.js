6c9a05b23c4d784b335a7806a8f6b2cd2a54cacd
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-04-19 18:26:47 -0600

    Add inquirer code, create markdown file structure

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const mdGen = require ('./utils/generateMarkdown.js')
+
+const licenses = [
+    'MIT',
+    'APACHE 2.0',
+    'GPL 3.0',
+    'BSD 3',
+    'None'
+];
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'githubUsername'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'emailAddress'
+    },
+    {
+        type: 'input',
+        message: 'What is your project\'s name?',
+        name: 'projectName'
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project:',
+        name: 'projectDescription'
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        choices: licenses,
+        name: 'projectLicense',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+        name: 'projectDependencies'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+        name: 'projectTests'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'projectUsage'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'projectContributing'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(response => {
+            // console.log(response);
+            console.log('Generating README...');
+            return;
+        });
+}
 
 // Function call to initialize app
 init();
+// const myObj = {title: 'Hello!'};
+// console.log(mdGen(myObj));
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,8 +15,58 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+![something license](https://img.shields.io/badge/license-MIT-blue.svg)
 
-`;
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+code goes here
+\`\`\`
+
+## Usage
+
+usage instructions go here
+
+## License
+
+This project is licensed user something license.
+
+## Contributing
+
+some contributing info goes here
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+code goes here
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at email@domain.com. You can find more of my work at [githubusername](https://github.com/githubusername).
+  `;
 }
 
 module.exports = generateMarkdown;
